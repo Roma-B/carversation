4f0a92fa2f2dd5c2084b4351516e62b9cd1689e2
GURURCB <gururaj.c_b@mercedes-benz.com>
2025-07-25 15:35:08 +0530

    initial commit

---
+++ carversation-app/src/main/java/com/mercedesbenz/carversation/CarversationApplication.java
@@ -0,0 +1,11 @@
+package com.mercedesbenz.carversation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarversationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarversationApplication.class, args);
+    }
+}
